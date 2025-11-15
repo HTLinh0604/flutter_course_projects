f98d60d237afd24a46a37aacf779bece79c7dbe9
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-15 17:02:40 +0700

    up ex 4

--- set_and_map.dart
+++ set_and_map.dart
@@ -1,8 +1,36 @@
 void main(){
-  var mySet ;
+  Set<String> set1 = {'apple', 'banana', 'cherry'};
+  var set2 = <int>{1,2,3};
+  Set<String> set3 =Set();
+  var set4 = Set<int>.from([1,2,2,3]);
 
-  var myMap = {
-    'name': 'linh',
-    'age': 21,
-  };
+  var set5 = {1,2,3};
+  var set6= {3,4,5};
+
+  var union = set5.union(set6); // hop 2 set}
+  var intersection = set5.intersection(set6); // giao
+  var difference = set5.difference(set6); // hieu
+  
+  // bien doi
+  var list = set1.toList(); // chuyen thanh list
+  var newSet = Set<String>.from(set1); // tao set moi
+  // loc va anh xa 
+  var filtered = set1.where((e) => e.startsWith('A')); // loc 
+  var mapped = set1.map((e) => e.toLowerCase()); // chuyen thanh chu thuong
+
+  // xu ly tung phan tu
+  set1.forEach((element){
+    print(element);
+  });
+
+  // vi du 
+  // quan ly danh sach sinh vien
+  Set<String> friendIds = {'id1', 'id2', 'id3'};
+  //them sinh vien moi
+  friendIds.add('id4');
+  friendIds.add('id2'); // khong them duoc vi da ton tai
+// kiem tra sinh vien
+
+
+
 }
