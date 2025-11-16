b366bf22e89d45926d0548564bacafff568ce6cf
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-16 22:33:16 +0700

    upload ex 6

---
+++ runes_dart.dart
@@ -0,0 +1,39 @@
+void main(){
+  String str = 'hi';
+  Runes runes1 = str.runes;
+
+  Runes runes2 = Runes('\u2665'); // Unicode for '♥'
+
+  Runes runes3 = Runes('\u{1F600}'); // Unicode for '😀'
+
+  print(runes2);
+  print(runes3);
+
+  // cac phuong thuc va thuoc tinh
+  // chuyen doi
+  // tu runes qua string
+  String heartSymbol = String.fromCharCodes(runes2);
+  print(heartSymbol); // Output: ♥
+  // tu unicode sang string
+  String emoji = String.fromCharCode(0x1F600); // '😀'
+  String symboil = String.fromCharCode(0x2665); // '♥'
+
+  print(emoji);
+  print(symboil);
+
+  // xu ly
+  print(runes1.length); // so luong unicode trong chuoi
+  print(runes1.first); // lay unicode dau tien
+  print(runes1.last); // lay unicode cuoi cung
+
+  // duyet qua tung diem ma
+  runes1 = "xin chao cac ban!, toi la dev flutter".runes;
+  runes1.forEach((int rune){
+    print('unicode: $rune, character: ${String.fromCharCode(rune)}');
+  });
+
+  //kiem tra
+  print(runes1.isEmpty);
+  print(runes1.isNotEmpty);
+
+}
