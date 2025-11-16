134eca71f0b5f9b9a3d66233793e4f891b71d3ea
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-16 23:01:42 +0700

    update

---
+++ typedef.dart
@@ -0,0 +1,12 @@
+/*
+typedef la một cách để định nghĩa một kiểu hàm trong Dart. 
+Nó cho phép bạn tạo ra các tên kiểu tùy chỉnh cho các hàm có cùng chữ ký, 
+giúp mã của bạn trở nên rõ ràng và dễ bảo trì hơn.
+
+*/
+typedef Intlist = List<int> ;
+void main(){
+  Intlist l1=[1,2,3,4,5];
+  Intlist l2=[6,7,8,9,10];
+
+}
