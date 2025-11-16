11dd5574a426a9c101b6a17a3ac2f041e3447a1c
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-16 23:06:13 +0700

    update

--- typedef.dart
+++ typedef.dart
@@ -5,8 +5,12 @@ giúp mã của bạn trở nên rõ ràng và dễ bảo trì hơn.
 
 */
 typedef Intlist = List<int> ;
+
+typedef ListMapper<X>= Map<X, List<X>>;
 void main(){
   Intlist l1=[1,2,3,4,5];
   Intlist l2=[6,7,8,9,10];
 
+  Map<String, List<String>> m1={};
+  ListMapper<String> m2 ={};
 }
