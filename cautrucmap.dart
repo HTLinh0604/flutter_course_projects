f00eeb9b6d430b74b9e5d771b28328397a71ce0d
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-16 22:09:15 +0700

    up ex 5

---
+++ cautrucmap.dart
@@ -0,0 +1,15 @@
+// cau truc du lieu map (dictionary)
+void main(){
+  /*
+  -map la cau truc du lieu luu tru dang key-value
+  - moi key la duy nhat
+  - value co the trung lap
+  - key va value co the la nhieu kieu du lieu
+  */
+  Map<String, dynamic> user1= {
+    'name':'Alice',
+    'age':30,
+    'isStudent':true,
+    
+  };
+}
